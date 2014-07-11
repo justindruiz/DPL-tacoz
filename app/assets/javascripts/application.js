e2ff0e4e58fe38e7c80ed8d7efa87c9c550096a0
justindruiz <justinruizwrestling@gmail.com>
2014-07-11 12:31:28 -0600

    adding geocoding

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,51 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require gmaps
 //= require_tree .
+
+$(function() {
+  loadMap();
+});
+
+var loadMap = function() {
+  $map = $('#map');
+  if($map.length) {
+    var gmap = new GMaps({
+      div: '#map',
+      lat: $map.data('lat'),
+      lng: $map.data('lon')
+    });
+    gmap.addMarker({
+      lat: $map.data('lat'),
+      lng: $map.data('lon'),
+      title: 'Tacoz',
+      infoWindow: {
+        content: '<p>'+ $map.data('address') +'</p>'
+      }
+    });
+  }
+
+  var $fullMap = $('#full-map');
+  if($fullMap.length) {
+    $.getJSON('/locations.json', function(locations) {
+      var defaultLocation = locations[0];
+      var gmap = new GMaps({
+        div: '#full-map',
+        lat: defaultLocation.latitude,
+        lng:defaultLocation.longitude
+      });
+
+      $(locations).each(function(index, location) {
+        gmap.addMarker({
+          lat: location.latitude,
+          lng: location.longitude,
+          title: 'Tacoz',
+          infoWindow: {
+            content: '<p>'+ location.address +'</p>'
+          }
+        });
+      });
+    });
+  }
+};
