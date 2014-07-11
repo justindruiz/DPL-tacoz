abb9b9a2f6b852c5be9d6d37ab738fc0d3856b6a
justindruiz <justinruizwrestling@gmail.com>
2014-07-11 10:58:09 -0600

    implememted location view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post '/contact-submit' => 'site#contact_submit', as: :submit_contact
   resources :menu_items, only: [:index, :show], path: 'our-food'
   get '/our-vegetarian-food' => 'menu_items#vegetarian'
+  resources :locations, only: [:index, :show]
 
   namespace :admin do
     get '/' => 'base#index'
