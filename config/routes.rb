2075c86e4b7f103aedffdb2a2fd08c01200a1d89
justindruiz <justinruizwrestling@gmail.com>
2014-07-21 09:44:06 -0600

    ominauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: 'omniauth_callbacks'}
   root 'site#index'
   get '/about' => 'site#about'
   get '/contact' => 'site#contact'
